eea70703ef8444da132834fecdb4f31d96b64658
Yerim Lee <rimmy2002@gmail.com>
2020-04-08 17:38:33 -0700

    Changed all data.text to str(data) and assigned a new variable name to refer to the str data instead of reusing the same variable name for the HTML. This change is to fix the issue where .text returns empty string on other machines.

--- data_pipelines/kr_covid_data/__init__.py
+++ data_pipelines/kr_covid_data/__init__.py
@@ -50,12 +50,16 @@ def _save(file_out):
     soup = BeautifulSoup(_get_url_data().decode('utf-8'), 'lxml')
     all_data = soup.find_all('script')
 
+    search_str = 'chartForDomestic":'
+
     for data in all_data:
-        if 'chartForDomestic' in str(data):
-            all_data = data
+        # save str version (so that I don't redo this every time)
+        data_str = str(data)
+        if search_str in data_str:
+            break
 
     # pull out the city level data
-    city_data = all_data.text.split('chartForDomestic":')[1]
+    city_data = data_str.split(search_str)[1]
     city_data = city_data.split(',"statByKrLocation')[0]
     city_data = ast.literal_eval(city_data)
 
@@ -68,11 +72,11 @@ def _save(file_out):
     city_data = {eng: city_data[kor] for kor, eng in labels}
 
     # pull out the testing data
-    testing = all_data.text.split('krTesting":')[1].split(',"age')[0]
+    testing = data_str.split('krTesting":')[1].split(',"age')[0]
     testing = ast.literal_eval(testing)
 
     # pull out the aggregated nation wide data
-    kr_total = all_data.text.split('{"KR":')[1].split(',"global')[0]
+    kr_total = data_str.split('{"KR":')[1].split(',"global')[0]
     kr_total = ast.literal_eval(kr_total)
 
     kr_data = {'cities': city_data, 'total': kr_total, 'testing': testing}
